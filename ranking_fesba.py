dd6ddb9fc5ff35023d11858331ab1cec4992ecc5
SoyFonso PC <alfonsopingar@gmail.com>
2021-11-17 00:30:58 +0100

    Converted the functions into general functions instead of purposed ones and placed (wrongly) the buttons without their functions.

--- ranking_fesba.py
+++ ranking_fesba.py
@@ -12,22 +12,27 @@ def getdata(url):
     soup = BeautifulSoup(r.text, 'html.parser')
     return soup
 
+'''
+Gets the structure of the page  inputted
+'''
+
 
 def ranking_link(soup):
     return soup.find_all('a', {'class': 'morelink'})[-1]['href']
 
+'''
+Gets the link for the whole ranking (not a specified category)
+'''
 
-def gotoim19(soup):
+
+def gotocategory(soup, category):
     a = str(soup.find_all('table', {'class': 'ruler'}))
-    b = a.partition('href="')[2]
-    return urljoin('https://www.badminton.es/ranking/', b.partition('">')[0]).replace('amp;', '')
+    b = a.partition(f'">{category}')[0]
+    return urljoin('https://www.badminton.es/ranking/', b.split('<th colspan="11"><a href="')[-1]).replace('amp;', '')
 
-
-def gotoif19(soup):
-    a = str(soup.find_all('table', {'class': 'ruler'}))
-    b = a.partition('href="')[2]
-    c = b.partition('">IF Sub 19')[0]
-    return urljoin('https://www.badminton.es/ranking/', c.partition('<th colspan="11"><a href="')[2]).replace('amp;', '')
+'''
+Gets the link for the ranking of the specified category (referred as category), takes the data from the ranking page.
+'''
 
 
 def gotooviedo(soup):
@@ -36,53 +41,81 @@ def gotooviedo(soup):
     c = b.partition('Asturias')[2]
     return c.split('ogid')[1]
 
+'''
+Goes to the Oviedo team, now inside the specified category.
+'''
 
 def gotoplayer(soup, name):
     a = str(soup.find_all('table', {'class': 'ruler'}))
     b = a.partition(f'">{name.upper()}')[0]
     return urljoin('https://www.badminton.es/ranking/', b.split('href="')[-1].replace('amp;', ''))
 
+'''
+Goes to the specified player specified as player.
+'''
 
-def getposition(soup):
+
+def getposition(soup): #TODO fix this thing
     a = str(soup.find_all('table', {'class': 'ruler'}))
     b = a.partition('IM Sub 19</a></td><td></td><td class="rank"><div style="">')[2]
     c = a.partition('PABLO SERRANO TASSIS</a></td><td class="rank"><div style="">')[2]
     return b.partition('</div>')[0], c.partition('</div>')[0]
 
+'''
+Gets the position of the player on each category where he/she participates.
+'''
 
-def getfinalrankim():
+
+def getfinalrank():
     fesba_data = getdata(web_fesba)
     ranking = ranking_link(fesba_data)
     ranking_page = urljoin(web_fesba, ranking)
     ranking_page_data = getdata(ranking_page)
-    im19 = gotoim19(ranking_page_data)
-    im19_data = getdata(im19)
-    oviedo = im19 + '&ogid' + gotooviedo(im19_data)
+    category_link = gotocategory(ranking_page_data, categoria)
+    specified_category_data = getdata(category_link)
+    oviedo = category_link + '&ogid' + gotooviedo(specified_category_data)
     oviedo_data = getdata(oviedo)
     player = gotoplayer(oviedo_data, nombre)
-    print(player)
+
     player_data = getdata(player)
     final_rank = f'IM 19: {getposition(player_data)[0]} \nDM 19 Pablo Serrano: {getposition(player_data)[1]}'
     messagebox.showinfo(message=final_rank, title=f"Ranking de {nombre}")
 
 
-def getfinalrankif():
-    fesba_data = getdata(web_fesba)
-    ranking = ranking_link(fesba_data)
-    ranking_page = urljoin(web_fesba, ranking)
-    ranking_page_data = getdata(ranking_page)
-    if19 = gotoif19(ranking_page_data)
-    if19_data = getdata(if19)
-    oviedo = if19 + '&ogid' + gotooviedo(if19_data)
-    oviedo_data = getdata(oviedo)
-
 
 root = tkinter.Tk()
-root.config(width=300, height=200)
 root.title("Ranking ESP Junior")
-label = tkinter.Label(root, text='Selecciona jugador:')
-label.place(relx=0.5, rely=0.05, anchor='center')
-nombre = 'Alfonso Pinto'
-botonalfonso = ttk.Button(root, text=nombre, command=getfinalrankim)
-botonalfonso.place(x=50, y=50)
+
+window_width = 480
+window_height = 360
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+
+selectcategory = tkinter.Label(root, text='Selecciona categoria:')
+selectcategory.place(x=240 , y=30)
+
+botonsub19 = ttk.Button(root, text='Sub 19')
+botonsub19.place(x=48, y=36)
+
+botonsub17 = ttk.Button(root, text='Sub 17')
+botonsub17.place(x=432, y=36)
+
+botonsub15 = ttk.Button(root, text='Sub 15')
+botonsub15.place(x=240, y=180)
+
+botonsub13 = ttk.Button(root, text='Sub 13')
+botonsub13.place(x=48, y=324)
+
+botonsub11 = ttk.Button(root, text='Sub 11')
+botonsub11.place(x=432, y=324)
+
 root.mainloop()
+
+
